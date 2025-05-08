07cb195f01a2a6069541a18aeead5146f010ed79
Deivison Luan <37543265+DeivisonLuan@users.noreply.github.com>
2025-05-08 20:40:56 -0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+/*
+3 - Alterar projeto 1 para que sejam as duas tasks no nucleo 1, 
+    com a mesma prioridade, se alternando. Usar semaforo. 
+    O que ocorre se uma task tiver com prioridade maior que a outra?
+    Teste este cenario.
+*/
+
+#include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/semphr.h>
+
+SemaphoreHandle_t SMF;
+TaskHandle_t handlePWM_t, handleBlink_t, handletask_t;
+
+//Definição dos pinos
+#define LedG 2 //pino do led Verde
+#define LedR 5 //pino do led Amarelo
+#define ButtonPin 13 //Pino do botão
+
+//Rotina de interrupção
+void IRAM_ATTR Pressed(){
+  xSemaphoreGiveFromISR(SMF, NULL);
+}
+
+void handlePWM();
+void handleBlink();
+void handletask(void* z);
+
+
+void setup() {
+  //inicialização dos pinos
+  pinMode(LedG, OUTPUT);
+  pinMode(LedR, OUTPUT);
+  pinMode(ButtonPin, INPUT_PULLUP);
+
+  //inicia interrupção
+  attachInterrupt(ButtonPin, Pressed, FALLING);
+
+  //inicia semaforo binário
+  SMF = xSemaphoreCreateBinary();
+  xTaskCreatePinnedToCore(handletask, "handletask", 4096, NULL, 1, &handletask_t, 1);
+}
+
+void loop() {
+  
+}
+
+void handletask(void* z){
+
+}
+
+void handlePWM(){
+  while(true){
+    for(byte i=0; i<=255 ; i++){
+      analogWrite(LedG, i);
+      delay(50);
+    }
+    for(byte i=255; i>=0 ; i--){
+      analogWrite(LedG, i);
+      delay(50);
+    }
+  }
+}
+
+void handleBlink(){
+  digitalWrite(LedR, HIGH);
+  delay(300);
+  digitalWrite(LedR,LOW);
+  delay(300);
+}
